17b9c3c16b5cd963618159531a8f8f1dc2b68ed5
KoblizekXD <janprokupek@centrum.cz>
2025-04-15 20:38:48 +0200

    Add basic constants instructions

--- src/interpreter.c
+++ src/interpreter.c
@@ -13,10 +13,106 @@
 #define INSTRUCTION(NAME) static inline void handler_##NAME(VirtualMachine *vm, ThreadFrame *frame, ExStack *opstack, ExStack *lvars)
 #define HANDLER_FOR(INSN, HANDLER) case INSN: handler_##HANDLER(vm, frame, opstack, lvars); break;
 
-INSTRUCTION(new)
+INSTRUCTION(aconst_null)
+{
+    PushReference(opstack, NULL);
+}
+
+INSTRUCTION(iconst_m1)
+{
+    PushInt(opstack, -1);
+}
+
+INSTRUCTION(iconst_0)
+{
+    PushInt(opstack, 0);
+}
+
+INSTRUCTION(iconst_1)
+{
+    PushInt(opstack, 1);
+}
+
+INSTRUCTION(iconst_2)
+{
+    PushInt(opstack, 2);
+}
+
+INSTRUCTION(iconst_3)
+{
+    PushInt(opstack, 3);
+}
+
+INSTRUCTION(iconst_4)
+{
+    PushInt(opstack, 4);
+}
+
+INSTRUCTION(iconst_5)
+{
+    PushInt(opstack, 5);
+}
+
+INSTRUCTION(lconst_0)
+{
+    PushLong(opstack, 0);
+}
+
+INSTRUCTION(lconst_1)
+{
+    PushLong(opstack, 1);
+}
+
+INSTRUCTION(fconst_0)
+{
+    PushFloat(opstack, 0.0f);
+}
+
+INSTRUCTION(fconst_1)
+{
+    PushFloat(opstack, 1.0f);
+}
+
+INSTRUCTION(fconst_2)
+{
+    PushFloat(opstack, 2.0f);
+}
+
+INSTRUCTION(dconst_0)
+{
+    PushDouble(opstack, 0.0);
+}
+
+INSTRUCTION(dconst_1)
+{
+    PushDouble(opstack, 1.0);
+}
+
+INSTRUCTION(bipush)
+{
+    PushByte(opstack, (int8_t) Read8());
+}
+
+INSTRUCTION(sipush)
+{
+    uint16_t value = Read16();
+    PushShort(opstack, (int16_t) value);
+}
+
+INSTRUCTION(ldc)
+{
+    uint8_t index = Read8();
+
+}
+
+INSTRUCTION(ldc_w)
 {
     uint16_t index = Read16();
-    PushReference(opstack, Instantiate(vm, FindClass(vm, *frame->method->cf->constant_pool[index - 1].info._class.name)));
+}
+
+INSTRUCTION(ldc2_w)
+{
+    uint16_t index = Read16();
 }
 
 /**
@@ -37,26 +133,27 @@ Item *execute_internal(VirtualMachine *vm, ThreadFrame *frame, ExStack *opstack,
     while (frame->pc < (code->code + code->code_length)) {
         uint8_t opcode = Read8();
         switch (opcode) {
-            HANDLER_FOR(NEW, new)
-            case DUP:
-                PushStack(opstack, Copy(opstack->data[opstack->top - 1]));
-                break;
-            case INVOKESPECIAL: {
-                uint16_t index = Read16();
-                MemberRef ref = cf->constant_pool[index - 1].info.member_ref;
-                ClassFile *target = FindClass(vm, *ref.class_info->name);
-                Method *m = GetMethodByNameAndDescriptor(target, *ref.name_and_type->name, *ref.name_and_type->descriptor);
-                if (StringEquals(m->name, "<init>") && (vm->options->flags & OPTION_DISABLE_INIT) != 0) {
-                    break;
-                }
-                PushStack(opstack, InvokeMethod(vm, m));
-                break;
-            }
-            case ATHROW: {
-                ObjectRegion *reg = PopReference(opstack);
-                ThrowException(vm, reg->cf->name, "");
-                break;
-            }
+            case NOP: break;
+            HANDLER_FOR(ACONST_NULL, aconst_null);
+            HANDLER_FOR(ICONST_M1, iconst_m1);
+            HANDLER_FOR(ICONST_0, iconst_0);
+            HANDLER_FOR(ICONST_1, iconst_1);
+            HANDLER_FOR(ICONST_2, iconst_2);
+            HANDLER_FOR(ICONST_3, iconst_3);
+            HANDLER_FOR(ICONST_4, iconst_4);
+            HANDLER_FOR(ICONST_5, iconst_5);
+            HANDLER_FOR(LCONST_0, lconst_0);
+            HANDLER_FOR(LCONST_1, lconst_1);
+            HANDLER_FOR(FCONST_0, fconst_0);
+            HANDLER_FOR(FCONST_1, fconst_1);
+            HANDLER_FOR(FCONST_2, fconst_2);
+            HANDLER_FOR(DCONST_0, dconst_0);
+            HANDLER_FOR(DCONST_1, dconst_1);
+            HANDLER_FOR(BIPUSH, bipush);
+            HANDLER_FOR(SIPUSH, sipush);
+            HANDLER_FOR(LDC, ldc);
+            HANDLER_FOR(LDC_W, ldc_w);
+            HANDLER_FOR(LDC2_W, ldc2_w);
             default:
                 ThrowException(vm, "java/lang/InternalError", "Unresolved instruction: %s - 0x%X", GetInstructionName(opcode), opcode);
                 break;
